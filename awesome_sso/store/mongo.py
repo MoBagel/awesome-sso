3b7ebadc567baf9c265ea4c1dc896ca047e41a94
Schwannden Kuo <schwannden@mobagel.com>
2021-11-30 17:08:32 +0800

    release 0.1.5 and use beanie

--- awesome_sso/store/mongo.py
+++ awesome_sso/store/mongo.py
@@ -1,12 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from odmantic import AIOEngine
 
 
 class MongoDB:
-    def __init__(
-        self, host: str, port: int, username: str, password: str, database: str
-    ):
+    def __init__(self, host: str, port: int, username: str, password: str):
         self.client = AsyncIOMotorClient(
             host=host, port=port, username=username, password=password
         )
-        self.engine = AIOEngine(motor_client=self.client, database=database)
