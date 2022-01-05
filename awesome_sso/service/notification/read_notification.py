582303e40b37cbed8a2b6152a0d803a8c1eb8797
Chloe <chloechen@mobagel.com>
2022-01-05 17:40:47 +0800

    [FEATURE][WS-679] add notificaiton schema, sync_notificaiton and read_notification

---
+++ awesome_sso/service/notification/read_notification.py
@@ -0,0 +1,22 @@
+import requests
+from beanie import PydanticObjectId
+from service.notification.schema import UserNotificationListResponse
+
+from awesome_sso.service.settings import Settings
+
+
+async def read_notification(
+    sso_user_id: PydanticObjectId, notification_id: PydanticObjectId
+) -> UserNotificationListResponse:
+    resp = requests.post(
+        "%s/notification" % Settings.sso_domain,
+        params={"user_id": str(sso_user_id), notification_id: str(notification_id)},
+        timeout=5,
+    )
+    resp.close()
+    if resp.status_code / 2 != 100:
+        raise RuntimeError(
+            "sync notification with sso failed. %s: "
+            + str(resp.content.decode("utf-8"))
+        )
+    return resp.json()
