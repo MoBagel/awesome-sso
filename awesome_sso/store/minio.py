a11f92de2dcd32f3b04a2879f72347a5838dd0ae
folderplus <33322004+folderplus@users.noreply.github.com>
2022-01-11 21:26:40 +0800

    Feature/minio new features (#16)
    * [Feature] list buckets, objects
    * [Feature] remove objects and download object
    * [Feature] fput add exclude_files
    * get_df date_column_list variable type
    * add remov_objects unit test

--- awesome_sso/store/minio.py
+++ awesome_sso/store/minio.py
@@ -3,7 +3,8 @@ import json
 from io import BytesIO, StringIO
 from logging import Logger
 from os import path
-from typing import IO, Optional
+from pathlib import Path
+from typing import IO, List, Optional
 
 import pandas as pd
 from fastapi import UploadFile
@@ -40,11 +41,32 @@ class MinioStore:
         else:
             self.logger.info("bucket '%s' exists", self.bucket)
 
-    def fput(self, name: str, file_path: str):
+    def list_buckets(self):
+        """List information of all accessible buckets with text."""
+        return [x.name for x in self.client.list_buckets()]
+
+    def list_objects(self, prefix: str = None, recursive: bool = False):
+        """Lists object information of a bucket with text."""
+        return [
+            x.object_name
+            for x in self.client.list_objects(
+                self.bucket, prefix=prefix, recursive=recursive
+            )
+        ]
+
+    def fput(self, name: str, file_path: str, exclude_files: List[str] = []):
+        """Uploads data from a file/folder to an object in a bucket."""
         if path.isdir(file_path):
             for local_file in glob.glob(file_path + "/**"):
+                file_name = Path(local_file).name
+                remote_path = path.join(name, file_name)
+
+                if file_name in exclude_files:
+                    self.logger.info(f"exclude: {local_file}")
+                    continue
+
                 if not path.isfile(local_file):
-                    self.fput(local_file, name + "/" + path.basename(local_file))
+                    self.fput(remote_path, local_file, exclude_files)
                 else:
                     remote_path = path.join(name, local_file[1 + len(file_path) :])
                     self.client.fput_object(self.bucket, remote_path, local_file)
@@ -58,27 +80,28 @@ class MinioStore:
         length: Optional[int] = None,
         content_type: str = "application/octet-stream",
     ):
+        """Uploads data from a stream to an object in a bucket."""
         if not length:
             length = len(data.read())
             data.seek(0)
+
         self.client.put_object(
             self.bucket, name, data, length, content_type=content_type
         )
 
     def put_as_json(self, name: str, data: dict):
-        data_json = json.dumps(data)
-        data_bytes = data_json.encode("utf-8")
+        """Uploads data from a json to an object in a bucket."""
+        data_bytes = json.dumps(data).encode("utf-8")
         data_byte_stream = BytesIO(data_bytes)
-        length = len(data_byte_stream.read())
-        data_byte_stream.seek(0)
-        self.client.put_object(
-            self.bucket, name, data_byte_stream, length, content_type="application/json"
-        )
+
+        self.put(name, data_byte_stream, content_type="application/json")
 
     def get(self, name: str):
+        """Gets data of an object."""
         return self.client.get_object(self.bucket, name)
 
     def exists(self, name: str) -> bool:
+        """Check if object or bucket exist."""
         try:
             self.client.stat_object(self.bucket, name)
             return True
@@ -86,23 +109,22 @@ class MinioStore:
             return False
 
     def remove_dir(self, folder: str):
+        """Remove folder."""
         self.logger.warning("removing %s", folder)
-        objects_to_delete = self.client.list_objects(
-            self.bucket, prefix=folder, recursive=True
-        )
-        object_names_to_delete = [x.object_name for x in objects_to_delete]
-        delete_objects = [DeleteObject(name) for name in object_names_to_delete]
-        self.logger.warning("Removing: %s", object_names_to_delete)
-        for del_err in self.client.remove_objects(self.bucket, delete_objects):
-            self.logger.warning("Deletion Error: %s", del_err)
+        objects_to_delete = self.list_objects(prefix=folder, recursive=True)
+        self.logger.warning("Removing: %s", objects_to_delete)
+        self.remove_objects(objects_to_delete)
 
     def remove_object(self, name: str):
+        """Remove an object."""
         self.client.remove_object(self.bucket, name)
 
-    def upload_df(self, df: pd.DataFrame, name: str):
-        csv_bytes = df.to_csv(index=False).encode("utf-8")
-        csv_buffer = BytesIO(csv_bytes)
-        self.put(name, csv_buffer, content_type="application/csv")
+    def upload_df(self, name: str, data: pd.DataFrame):
+        """Uploads data from a pandas dataframe to an object in a bucket."""
+        data_bytes = data.to_csv(index=False).encode("utf-8")
+        data_byte_stream = BytesIO(data_bytes)
+
+        self.put(name, data_byte_stream, content_type="application/csv")
 
     def fget_df(
         self,
@@ -118,15 +140,16 @@ class MinioStore:
                 df = pd.read_csv(file_io, dtype=column_types, parse_dates=[date_column])
             file_io.close()
         except Exception as e:
-            logger.warning(UnprocessableEntity("unable to read csv %s" % str(e)))
+            self.logger.warning(UnprocessableEntity("unable to read csv %s" % str(e)))
             return None
         return df
 
-    def get_json(self, file_path: str) -> dict:
+    def get_json(self, name: str) -> dict:
+        """Gets data of an object and return a json."""
         try:
-            file_obj = self.get(file_path)
+            file_obj = self.get(name)
         except S3Error as e:
-            logger.warning(e)
+            self.logger.warning(e)
             return {}
         result = json.load(file_obj)
         file_obj.close()
@@ -134,17 +157,32 @@ class MinioStore:
         return result
 
     def get_df(
-        self, file_path: str, column_types: dict = {}, date_column: Optional[str] = None
+        self,
+        name: str,
+        column_types: dict = {},
+        date_column_list: List[str] = [],
     ) -> Optional[pd.DataFrame]:
+        """Gets data of an object and return a dataframe."""
         try:
-            file_obj = self.get(file_path)
+            file_obj = self.get(name)
         except S3Error as e:
-            logger.warning(e)
+            self.logger.warning(e)
             return None
-        if date_column is None:
+
+        if not date_column_list:
             df = pd.read_csv(file_obj, dtype=column_types)
         else:
-            df = pd.read_csv(file_obj, parse_dates=[date_column], dtype=column_types)
+            df = pd.read_csv(file_obj, parse_dates=date_column_list, dtype=column_types)
         file_obj.close()
         file_obj.release_conn()
         return df
+
+    def remove_objects(self, names: list):
+        """Remove objects."""
+        objects_to_delete = [DeleteObject(name) for name in names]
+        for del_err in self.client.remove_objects(self.bucket, objects_to_delete):
+            self.logger.warning("Deletion Error: %s", del_err)
+
+    def download(self, name: str, file_path: str):
+        """Downloads data of an object to file."""
+        self.client.fget_object(self.bucket, name, file_path)
--- tests/datastore/test_minio.py
+++ tests/datastore/test_minio.py
@@ -1,12 +1,12 @@
-import json
+import os
 import tempfile
 
 from fastapi import UploadFile
 
 
 def test_bucket_creation(minio_store):
-    buckets = minio_store.client.list_buckets()
-    assert 'test' in buckets
+    buckets = minio_store.list_buckets()
+    assert "test" in buckets
 
 
 def test_fput(minio_store, test_string):
@@ -20,11 +20,18 @@ def test_fput(minio_store, test_string):
     begotten.release_conn()
 
     with tempfile.TemporaryDirectory() as dir:
-        with tempfile.NamedTemporaryFile(dir=dir) as file:
-            file.write(test_string)
-            file.flush()
-            minio_store.fput(dir, dir)
-    assert minio_store.exists(file.name)
+        with tempfile.NamedTemporaryFile(dir=dir) as file1:
+            file1.write(test_string)
+            file1.flush()
+            with tempfile.NamedTemporaryFile(dir=dir) as file2:
+                file_name2 = file2.name[1 + len(dir) :]
+                file2.write(test_string)
+                file2.flush()
+                with tempfile.TemporaryDirectory(dir=dir) as tmp_dir:
+                    minio_store.fput(dir, dir, exclude_files=[file_name2])
+
+    assert minio_store.exists(file1.name)
+    assert minio_store.exists(file2.name) is False
 
 
 def test_put(minio_store, test_string):
@@ -40,33 +47,50 @@ def test_put(minio_store, test_string):
 
 
 def test_put_and_get_json(minio_store, test_dict):
-    minio_store.put_as_json('dict.json', test_dict)
-    assert minio_store.exists('dict.json')
-    begotten = minio_store.get_json('dict.json')
-    assert begotten['test_string'] == test_dict['test_string']
-    begotten = minio_store.get_json('non_exist.json')
+    minio_store.put_as_json("dict.json", test_dict)
+    assert minio_store.exists("dict.json")
+    begotten = minio_store.get_json("dict.json")
+    assert begotten["test_string"] == test_dict["test_string"]
+    begotten = minio_store.get_json("non_exist.json")
     assert len(begotten) == 0
 
 
-def test_remove_object_and_dir(minio_store, test_dict):
-    minio_store.put_as_json('dict.json', test_dict)
-    minio_store.put_as_json('tmp/dict.json', test_dict)
-    assert minio_store.exists('dict.json')
-    assert minio_store.exists('tmp/dict.json')
-    minio_store.remove_object('dict.json')
-    assert minio_store.exists('dict.json') is False
-    minio_store.remove_dir('tmp')
-    assert minio_store.exists('tmp/dict.json') is False
-
-
-def test_upload_and_get_df(minio_store, test_dataframe):
-    minio_store.upload_df(test_dataframe, "test.csv")
+def test_put_get_and_download_df(minio_store, test_dataframe):
+    minio_store.upload_df("test.csv", test_dataframe)
     df = minio_store.get_df("test.csv")
     assert df.shape[0] == 100
-    df = minio_store.get_df("test.csv", date_column="column_4_date")
+    df = minio_store.get_df("test.csv", date_column_list=["column_4_date"])
     assert df.shape[0] == 100
-    df = minio_store.get_df("not_exist.csv", date_column="column_4_date")
+    df = minio_store.get_df("not_exist.csv", date_column_list=["column_4_date"])
     assert df is None
+
+    minio_store.download("test.csv", "test.csv")
+    assert os.path.exists("test.csv")
+    os.remove("test.csv")
+
+
+def test_remove_object_objects_and_dir(minio_store, test_dict):
+    for i in range(3):
+        minio_store.put_as_json(f"dict{i}.json", test_dict)
+    minio_store.put_as_json("tmp/dict.json", test_dict)
+
+    for i in range(3):
+        assert minio_store.exists(f"dict{i}.json")
+    assert minio_store.exists("tmp/dict.json")
+
+    # test remove object
+    minio_store.remove_object("dict0.json")
+    assert minio_store.exists("dict0.json") is False
+
+    # test remove objects
+    minio_store.remove_objects(["dict1.json", "dict2.json", "dict3.json"])
+
+    assert minio_store.exists("dict1.json") is False
+    assert minio_store.exists("dict2.json") is False
+
+    # test remove directory
+    minio_store.remove_dir("tmp")
+    assert minio_store.exists("tmp/dict.json") is False
 
 
 async def test_fget_df(minio_store, test_dataframe):
