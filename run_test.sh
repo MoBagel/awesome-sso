8442e745faf820900b36a2a51f77e9ac38f10511
Schwannden Kuo <schwannden@mobagel.com>
2022-07-20 13:53:16 +0800

    improve test coverage

--- run_test.sh
+++ run_test.sh
@@ -1,33 +1,15 @@
 #!/usr/bin/env bash
 
-function run_minio_server() {
-    if [ ! -f tests/minio ]; then
-        wget --quiet --output-document tests/minio https://dl.min.io/server/minio/release/linux-amd64/minio
-        chmod +x tests/minio
-    fi
-
-    export MINIO_ACCESS_KEY=minio
-    export MINIO_SECRET_KEY=minio123
-    export MINIO_ADDRESS=0.0.0.0:9002
-    tests/minio server /tmp/fs --address $MINIO_ADDRESS >tests/minio.log 2>&1 &
-}
-
 function run_mongo() {
    docker run --name awesome-mongo --rm -p 27000:27017 -d mongo:4.4.10
 }
 
 if [ -z ${SERVER_ENDPOINT+x} ]; then
-    run_minio_server
-    MINIO_PID=$!
-    trap 'kill -9 ${MINIO_PID} 2>/dev/null' INT
     trap 'docker rm -f awesome-mongo' INT
     run_mongo
 fi
 
 export MONGODB_DNS=mongodb://localhost:27000/beanie_db
 pytest --cov=awesome_sso tests --cov-report term-missing
-if [ -n "$MINIO_PID" ]; then
-    kill -9 "$MINIO_PID" 2>/dev/null
-    docker rm -f awesome-mongo
-fi
+docker rm -f awesome-mongo
 
--- tests/service/test_user.py
+++ tests/service/test_user.py
@@ -1,6 +1,8 @@
 import pytest
+from awesome_exception.exceptions import InternalServerError
 
 from awesome_sso.service.user.schema import AwesomeUser
+from awesome_sso.service.user.sync_user import sync_user
 from tests.conftest import init_mongo
 from tests.service.model import FantasticUser, UserWithExtraParameter
 
@@ -32,3 +34,9 @@ async def test_child_user_with_extra_param(register_model):
     assert user.email == register_model.email
     assert user.sso_user_id == register_model.sso_user_id
     assert user.extra.nickname == register_model.name
+
+
+async def test_sync_user(register_model):
+    user: FantasticUser = await FantasticUser.register(register_model)
+    with pytest.raises(InternalServerError):
+        await sync_user(user)
